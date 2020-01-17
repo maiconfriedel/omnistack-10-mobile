1a73f2cb88788c86ea7c88eb53646cd4808efbb5
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-01-17 14:35:23 -0300

    Added User Icon in Header
    - Added user icon in the header right position

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { FontAwesome } from "@expo/vector-icons";
 
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
@@ -22,6 +25,30 @@ const Routes = createAppContainer(
     },
     {
       defaultNavigationOptions: {
+        headerRight() {
+          return (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                paddingHorizontal: 10,
+                height: 40,
+                marginRight: 10,
+                borderRadius: 20,
+                justifyContent: "flex-end",
+                alignItems: "center",
+                backgroundColor: "#8E4DFF",
+                flexDirection: "row"
+              }}
+            >
+              <Text style={{ color: "#fff" }}>Maicon</Text>
+              <FontAwesome
+                style={{ color: "#fff", marginLeft: 10 }}
+                name="user"
+                size={25}
+              />
+            </TouchableOpacity>
+          );
+        },
         headerBackTitleVisible: false,
         headerStyle: {
           backgroundColor: "#7d40e7"
